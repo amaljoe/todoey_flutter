fff2573488e9826e34392eee6752da1e6a2e73dc
Amal Joe R S <amaljoe88@gmail.com>
2020-09-13 18:32:45 +0530

    Task updating with user click

--- lib/components/task_tile.dart
+++ lib/components/task_tile.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isTaskChecked = false;
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
         'This is a task',
-        style: TextStyle(fontSize: 20),
+        style: TextStyle(
+            fontSize: 20,
+            decoration: isTaskChecked
+                ? TextDecoration.lineThrough
+                : TextDecoration.none),
       ),
-      trailing: Checkbox(value: false, onChanged: (value) {}),
+      trailing: TaskCheckbox(
+        isChecked: isTaskChecked,
+        onChanged: (value) {
+          setState(() {
+            isTaskChecked = value;
+          });
+        },
+      ),
     );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool isChecked;
+  final Function onChanged;
+
+  TaskCheckbox({this.isChecked, this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+        activeColor: Colors.lightBlueAccent,
+        value: isChecked,
+        onChanged: (value) {
+          onChanged(value);
+        });
   }
 }
